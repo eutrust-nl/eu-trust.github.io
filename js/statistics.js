70f1434024e614aeb4245082f45c95563b27d4f9
Arctic Crow (Daniel) <arctic-crow@web.de>
2018-03-30 20:04:17 +0200

    Initial Commit

--- js/statistics.js
+++ js/statistics.js
@@ -1,38 +1,70 @@
 const BASE_URL = "https://opendata.cbs.nl/ODataApi/odata/80518ENG/";
-const TYPED_DATA_SET = "TypedDataSet";
-const TABLE_INFOS = "TableInfos";
-const DATA_PROPERTIES = "DataProperties";
-const COUNTRIES = "Countries";
-const PERSONAL_CHARACTERISTICS = "PersonalCharacteristics";
+const URLS = {
+    TYPED_DATA_SET: "TypedDataSet",
+    TABLE_INFOS: "TableInfos",
+    DATA_PROPERTIES: "DataProperties",
+    COUNTRIES: "Countries",
+    PERSONAL_CHARACTERISTICS: "PersonalCharacteristics"
+};
 
+let totalJSONs = Object.keys(URLS).length;
+let loadedJSONs = 0;
+
+let rawData = {};
 let statistics;
 
 
 
 $(function() {
     getJSONs();
+
+
+
+    // Setup handlers
+    // Show/hide "back to top"
+    $(window).on( "scroll", function() {
+        let $topNav = $("#topNav");
+        if ($('html, body').scrollTop() > $("header").height()) {
+            $topNav.css("visibility", "visible");
+            $topNav.show("fast");
+        } else {
+            $topNav.hide("fast");
+        }
+    });
 });
 
 function getJSONs(callback) {
-    let done = 4;
-    console.log(done);
+    // Reset counter variable
+    loadedJSONs = 0;
+    $.each(URLS, function(key ,url) {
+        console.log("Started retrieving", url);
+        $.getJSON(BASE_URL + url,
+            function (data) {
+                console.log(url, "was retrieved");
+
+                // Add the data to the raw data pool
+                rawData[url] = data.value;
+
+                loadedJSONs ++;
+                console.log(loadedJSONs, totalJSONs, totalJSONs === loadedJSONs);
+                if (loadedJSONs === totalJSONs) {
+                    console.log(rawData);
+                }
+            })
+            .fail(function () {
+                console.error("An error occurred while trying to get", url);
+            });
+    });
 }
 
-// Scroll control
-$().scroll(function() {
-    if ($().scrollTop() > 100) {
-        $("#topNav").fadeIn();
-    } else {
-        $("#topNav").fadeOut();
-    }
-});
 
+
+// Scroll control
 // Page Navigation - js from https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
 function scrollToTop(id) {
-    let position = (id === undefined) ? 0 : $(id).position();
-    console.log(position, id);
+    let position = (id === undefined) ? 0 : $(id).position().top;
 
     $('html, body').animate({
-        scrollTo: position
+        scrollTop: position
     }, 200);
 }
